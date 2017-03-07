d66aad4a1888a915518da72e9c0b41b42d0769ea
czldl <czldl@tom.com>
2017-03-07 19:32:41 +0800

    Prime类和Time类的优化

--- cppse161/PrimeGenerator/PrimeGenerator.cpp
+++ cppse161/PrimeGenerator/PrimeGenerator.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 PrimeGenerator::PrimeGenerator(int l, int u)
-	:lowerNaturalNumber(l), upperNaturalNumber(u), count(0) {}
+	:lowerNaturalNumber(l), upperNaturalNumber(u), count(0){}
 
 void PrimeGenerator::getPrimeSequence() {
 	cout << lowerNaturalNumber << "  到  " << upperNaturalNumber << "之间的素数序列为：" << endl;
@@ -19,8 +19,9 @@ void PrimeGenerator::getPrimeSequence() {
 			continue;// 短路一次循环中后面的部分
 		if (isPrime(m)) {
 			//cout << m << ", ";
+			arraySavePrimes[count] = m;
 			count++;
-			printPrime(1, m);
+			printPrime1( m);
 		}
 	}
 	cout << "\n共： " << count << " 质数！" << endl;
@@ -57,25 +58,59 @@ void PrimeGenerator::getPrimeSequenceNestFor() {
 		}
 		if (flag == true) {
 			//std::cout<< m<<", ";
+			arraySavePrimes[count] = m;
 			count++;
-			printPrime(2, m);
+			printPrime2(m);
 		}
 	}
 	cout << "\n共： " << count << " 质数！" << endl;
 }
 
-void PrimeGenerator::printPrime(int style, int data) {
+void PrimeGenerator::printPrime1(int data) {
+	static int c = 0;//col
+	cout << setw(3) << data << " ";
+	c++;
+	if (c % 5 == 0)
+		cout << endl;
+}
+void PrimeGenerator::printPrime2(int data) {
 	static int c = 0;//col
 	static int r = 1;//row
-	static int n = 1;
-	if (style == 1) {
-		cout << setw(3) << data << " ";
-		c++;
-		if (c % 5 == 0)
-			cout << endl;
+	static int n = 1;//col_num
+	cout << setw(3) << data << " ";
+	c++;
+	if (n <= c) {
+		cout << endl;
+		c = 0;
+		n = ++r;
 	}
-	else if (style == 2) {
-		cout << setw(3) << data << " ";
+}
+void PrimeGenerator::printPrime3() {
+	int c = 0;//col
+	int r = 1;//row
+	int n = 0;//row num
+
+	int l = 0;//level
+
+	for (int i = 0; i < count; i++) {
+		//cout << setw(3) << arraySavePrimes[i] << " ";
+		c++;
+		if (n <= c) {
+			//cout << endl;
+			c = 0;
+			n = ++r;
+			l++;
+		}
+	}
+	c = 0; r = 1; n = 1;//三个数字复位
+	int m = l;
+	for (int i = 0; i < count; i++) {
+		if (m == l-r+1) {
+			for (int j = 0; j <= m; j++)
+				cout << setw(2) << " ";
+			m--;
+		}
+		cout << setw(3) << arraySavePrimes[i] << " ";
 		c++;
 		if (n <= c) {
 			cout << endl;
@@ -83,6 +118,4 @@ void PrimeGenerator::printPrime(int style, int data) {
 			n = ++r;
 		}
 	}
-	else
-		cout << setw(3) << data << " ";
 }
--- cppse161/PrimeGenerator/PrimeGenerator.h
+++ cppse161/PrimeGenerator/PrimeGenerator.h
@@ -6,15 +6,65 @@
 */
 #include<iostream>
 #include<iomanip>
+#include<new> 
+
+const int MAXPRIME = 256;
+
 
 class PrimeGenerator {
+
 	int lowerNaturalNumber;
 	int upperNaturalNumber;
-	int count;
+	int count;//素数个数
+	int arraySavePrimes[MAXPRIME];//保存素数
 public:
+	friend class savePrimeArray;
+
 	PrimeGenerator(int l, int u);
 	void getPrimeSequence();
 	void getPrimeSequenceNestFor();
 	bool isPrime(int number);
-	void printPrime(int style, int number);
+	void printPrime1(int number);
+	void printPrime2(int number);
+	void printPrime3();
+	int getCount() { return count; }
+};
+
+class savePrimeArray {
+	int count;
+	int sArray[MAXPRIME];
+	int *dArray;
+
+public:
+	savePrimeArray(const PrimeGenerator& pg) {
+		dArray = NULL;
+		count = pg.count;
+		if (count > MAXPRIME){
+			std::cout << "太多素数，空间已满！" << std::endl;
+			exit(0);
+		}
+		for (int i = 0; i < count; i++)
+			sArray[i] = pg.arraySavePrimes[i];
+	}
+	savePrimeArray(const PrimeGenerator& pg, int c):count(c) {
+		dArray = new int[count];
+		if (!dArray)
+			throw - 1;
+		for (int i = 0; i < count; i++)
+			dArray[i] = pg.arraySavePrimes[i];
+	}
+	void printSPrime() {
+		for (int i = 0; i < count; i++) {
+			std::cout << sArray[i] << ", ";
+		}
+	}
+	void printDPrime() {
+		for (int i = 0; i < count; i++) {
+			std::cout << dArray[i] << ", ";
+		}
+	}
+	~savePrimeArray() {
+		if(dArray!=NULL)
+			delete[] dArray;
+	}
 };
