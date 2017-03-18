32bae5e48ffca7d1d736a2e6addcb12a2bc8c172
czldl <czldl@tom.com>
2017-03-18 20:54:19 +0800

    素数生成器的文件读写操作

--- cppse161/CPPIOStream/CPPIOStream.cpp
+++ cppse161/CPPIOStream/CPPIOStream.cpp
@@ -1,17 +1,15 @@
 // CPPIOStream.cpp : 定义控制台应用程序的入口点。
-//
 
-#include "stdafx.h"
 #include<fstream>
 #include<iostream>
 #include<iomanip>
-
+#include<cstdlib>
 using namespace std;
 
 class PrimeGenerator {
 
-	int lowerNaturalNumber;
-	int upperNaturalNumber;
+	int lowernum;
+	int uppernum;
 	int count;//素数个数
 	int *arraySavePrimes;//保存素数
 public:
@@ -23,31 +21,30 @@ public:
 };
 
 PrimeGenerator::PrimeGenerator(int l, int u)
-	:lowerNaturalNumber(l), upperNaturalNumber(u), count(0) {
-	arraySavePrimes = nullptr;
+	:lowernum(l), uppernum(u), count(0) {
+	arraySavePrimes = 0;
 }
 
 void PrimeGenerator::getPrimeSequence() {
-	cout << lowerNaturalNumber << "  到  " << upperNaturalNumber << "之间的素数序列为：" << endl;
-	if (lowerNaturalNumber > upperNaturalNumber) {	// 区间界限输入反时交换
-		int temp = lowerNaturalNumber;
-		lowerNaturalNumber = upperNaturalNumber;
-		upperNaturalNumber = temp;
+	cout << lowernum << "  " << uppernum << "之间的素数共有：";
+	if (lowernum > uppernum) {	// 区间界限输入反时交换
+		int temp = lowernum;
+		lowernum = uppernum;
+		uppernum = temp;
 	}
-	arraySavePrimes = new int[upperNaturalNumber- lowerNaturalNumber];
-	for (int m = lowerNaturalNumber; m <= upperNaturalNumber; m++) {	// 循环控制
+	arraySavePrimes = new int[uppernum - lowernum];
+	for (int m = lowernum; m <= uppernum; m++) {	// 循环控制
 		if (m < 2)
 			continue;// 短路一次循环中后面的部分
 		if (isPrime(m)) {
-			cout << setw(3) << m << ", ";
 			arraySavePrimes[count] = m;
 			count++;
 		}
 	}
-	cout << "\n共： " << count << " 质数！" << endl;
+	cout << count << " 个素数。" << endl;
 }
 
-bool PrimeGenerator::isPrime(int number) {
+bool inline PrimeGenerator::isPrime(int number) {
 	for (int m = 2; m < number; m++) {
 		if (number % m == 0) {
 			return false;// 发现number能被一个数整除，就断定它不是素数
@@ -57,92 +54,71 @@ bool PrimeGenerator::isPrime(int number) {
 }
 
 void PrimeGenerator::printPrime() {
-	int c = 0;//col
-	int r = 1;//row
-	int n = 0;//row num
+	int col = 0;//col
+	int row = 1;//row
+	int nrow = 0;//row num
 
-	int l = 0;//level
+	int level = 0;//level
 
 	for (int i = 0; i < count; i++) {
-		//cout << setw(3) << arraySavePrimes[i] << " ";
-		c++;
-		if (n <= c) {
-			//cout << endl;
-			c = 0;
-			n = ++r;
-			l++;
+		col++;
+		if (nrow <= col) {
+			col = 0;
+			nrow = ++row;
+			level++;
 		}
 	}
-	c = 0; r = 1; n = 1;//三个数字复位
-	int m = l;
+	col = 0; row = 1; nrow = 1;//三个数字复位
+	int m = level;
 	for (int i = 0; i < count; i++) {
-		if (m == l - r + 1) {
+		if (m == level - row + 1) {
 			for (int j = 0; j <= m; j++)
 				cout << setw(2) << " ";
 			m--;
 		}
 		cout << setw(3) << arraySavePrimes[i] << " ";
-		c++;
-		if (n <= c) {
+		col++;
+		if (nrow <= col) {
 			cout << endl;
-			c = 0;
-			n = ++r;
+			col = 0;
+			nrow = ++row;
 		}
 	}
 	cout << endl;
 }
 void PrimeGenerator::savePrime() {
-	int c = 0;//col
-	int r = 1;//row
-	int n = 0;//row num
-
-	int l = 0;//level
-
-	for (int i = 0; i < count; i++) {
-		//cout << setw(3) << arraySavePrimes[i] << " ";
-		c++;
-		if (n <= c) {
-			//cout << endl;
-			c = 0;
-			n = ++r;
-			l++;
-		}
-	}
-	c = 0; r = 1; n = 1;//三个数字复位
-	int m = l;
 	string saveprimefilename = "saveprime.txt";
-	ofstream out(saveprimefilename);	// 创建输出流并打开文件            	if (!out) {		cerr << "不能打开saveprime文件！\n";		exit(0);	}
+	ofstream out(saveprimefilename.c_str());	// 创建输出流并打开文件            
+	if (!out) {
+		cerr << "不能打开saveprime文件！\n";
+		exit(0);
+	}
+	out << lowernum << " " << uppernum << endl;
 	for (int i = 0; i < count; i++) {
-		if (m == l - r + 1) {
-			for (int j = 0; j <= m; j++)
-				out << setw(2) << " ";
-			m--;
-		}
 		out << setw(3) << arraySavePrimes[i] << " ";
-		c++;
-		if (n <= c) {
-			out << endl;
-			c = 0;
-			n = ++r;
-		}
 	}
 	out << endl;
 	out.close();
 	system(saveprimefilename.c_str());
 }
 
-int main(){
-	string readprimefilename = "readprime.txt";
-	ifstream in(readprimefilename);	// 创建输入流并打开账文件  	if (!in) {		std::cerr << "不能打开readprime文件!\n";		exit(0);	}
-	int l, u;
-	in >> l >> u;
+int main(int argc, char *argv[]) {
+
+	string readprimefilename = "readprimelowup.txt";
+	ifstream in(readprimefilename.c_str());	// 创建输入流并打开账文件  
+	if (!in) {
+		std::cerr << "不能打开readprime文件!\n";
+		exit(0);
+	}
+	int low, up;
+	in >> low >> up;
 	in.close();
-	PrimeGenerator pg(l, u);
+	PrimeGenerator pg(low, up);
 	pg.getPrimeSequence();
 	pg.printPrime();
 
 	pg.savePrime();
 	system(readprimefilename.c_str());
 
-    return 0;
+	return 0;
 }
