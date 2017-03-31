82dfcb85faf3fd6a20100cde30a68ebb8c2cee7f
czldl <czldl@tom.com>
2017-03-31 08:39:50 +0800

    创建CESMS库

---
+++ CESMS/LibCESMS/Employee.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+
+#define DLL_IMPLEMENT 
+
+#include "Employee.h"
+using namespace std;
+
+void Employee::getPay(){
+	//cout<<"职员工资：10000元"<<endl;
+}
+Employee::Employee() {}
---
+++ CESMS/LibCESMS/Manager.cpp
@@ -0,0 +1,11 @@
+#include "stdafx.h"
+
+#define DLL_IMPLEMENT 
+
+#include "Manager.h"
+using namespace std;
+
+void Manager::getPay(){
+	cout<<"经理工资：10000元"<<endl;//更多的功能在此展开
+}
+Manager::Manager() {}
