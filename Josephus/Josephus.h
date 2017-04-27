c6980031c812cc9276dba5d3021a423bb6c927ee
czldl <czldl@tom.com>
2017-04-27 12:37:09 +0800

    list约瑟夫问题

---
+++ Josephus/Josephus.h
@@ -0,0 +1,5 @@
+#ifndef JOSEPHUS_H
+#define JOSEPHUS_H
+#include <list>using namespace std;class Josephus{	private:		int listSize;	// 元素个数		int steps;		// 报数个数		int first;		// 第1人报的数		static list<int> josephusList;// 链表	public:		Josephus(int,int,int);	// 构造函数		void deleteElement();	// 删除元素		void display();			// 显示全部元素};
+#endif
+
