7797306cd78d93061f73337e715946c1b25d15f9
czldl <czldl@tom.com>
2017-11-07 20:27:39 +0800

    随机抽取

--- SpeechGroup/main.cpp
+++ SpeechGroup/main.cpp
@@ -26,13 +26,13 @@ int main (int argc, char *argv[]) {
 	const int t=7;
 	int s=rand()%t+1;
 	JosephusProblem jose(t,s,1);
-	cout << "G1项目组\n";
+	cout << "随机抽取G1项目组\n";
 	int r=jose.deleteElement();	
 	showGroup("projectgroup1.txt",r);
 	system("pause");
 	s=rand()%t+1;
 	JosephusProblem jose1(t,s,1);
-	cout << "G2项目组\n";
+	cout << "随机抽取G2项目组\n";
 	r=jose1.deleteElement();	
 	showGroup("projectgroup2.txt",r);
 	
